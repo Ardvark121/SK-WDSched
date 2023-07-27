502ac5484d4b08b705120d4a77edbbd284f58774
Shawn Kim <kimshawnj@gmail.com>
2023-07-27 18:10:36 -0500

    Added the comments on what the js does

--- Assests/js/script.js
+++ Assests/js/script.js
@@ -1,27 +1,23 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Set the variable that is linked in the html that will be appened to in order to add the time boxes
 var SchedContain = $("#Big-Container");
-const SKusrdatta = JSON.parse(localStorage.getItem("SKusrdatta")) || [];
-var timestart = 9;
 
+//Set the variable that is linked to local storage
+const SKusrdatta = JSON.parse(localStorage.getItem("SKusrdatta")) || [];
+//Set the variable that will be called on in order to time the box placments
+var timestart = 9;
+// Set the variable for the linked day.js
 var today = dayjs();
 var RawTime = today.format("h");
+// This turns the time into a readable integer
 var CurrentTime = parseInt(RawTime);
 if (today.format("a") == "pm") {
   CurrentTime = CurrentTime += 11;
 }
-
-console.log(CurrentTime);
 $(function (event) {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of thes
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Formats the data from the day.js and adds it to the p element under the title by id
   var Date = dayjs("2020-11-03").format("[Today is] dddd, MMMM D YYYY");
   $("#currentDay").text(Date);
+  // This creates new boxes and adds it to the local array and adds them to the page if nothing is saved
   if (SKusrdatta.length === 0) {
     timestart = 9;
     for (var i = 0; i < 12; i++) {
@@ -29,6 +25,7 @@ $(function (event) {
       OABlock.addClass("row time-block");
       const timesect = $("<div>");
       timesect.addClass("col-2 col-md-1 hour text-center py-3");
+      // converts the data from day.js back into string version of the time
       if (timestart < 12) {
         timesect.text(timestart + "AM");
       } else if (timestart == 12) {
@@ -44,6 +41,7 @@ $(function (event) {
       SKusrdatta.push(Timeblockval);
       OABlock.append(timesect);
       const BlockText = $("<textarea>");
+      // This uses compares the current time to the time in the box and colors it accordingly based on if the time is in the past present or future
       if (timestart < CurrentTime) {
         BlockText.addClass("col-8 col-md-10 description past");
       } else if (timestart == CurrentTime) {
@@ -63,10 +61,12 @@ $(function (event) {
       Blockbut.attr("aria-label", "save");
       OABlock.append(Blockbut);
       SchedContain.append(OABlock);
+      // This adjusts the timing varable set
       timestart++;
     }
+    // This creates the boxes already stored in the local array if it has something stored
   } else {
-    var timestart = 9;
+    timestart = 9;
     $.each(SKusrdatta, function (index, value) {
       const OABlock = $("<section>");
       OABlock.addClass("row time-block");
@@ -75,6 +75,7 @@ $(function (event) {
       timesect.text(value.time);
       OABlock.append(timesect);
       const BlockText = $("<textarea>");
+      // This uses compares the current time to the time in the box and colors it accordingly based on if the time is in the past present or future
       if (timestart < CurrentTime) {
         BlockText.addClass("col-8 col-md-10 description past");
       } else if (timestart == CurrentTime) {
@@ -98,21 +99,19 @@ $(function (event) {
     });
   }
 
+  // sets a variable to all the save buttons by class
   const saveBtn = $(".saveBtn");
+  // This is activated if the save button is pushed and adds the data from that into the local array
   saveBtn.on("click", function (event) {
+    // This gets the time and text from the other divs in the same div as the selected save button
     var es = $(this).siblings(".description").val();
     var et = $(this).siblings(".hour").text();
     const Timeblockval = {
       time: et,
       text: es,
     };
-
-    if (SKusrdatta.length === 0) {
-      SKusrdatta.push(Timeblockval);
-      let string = JSON.stringify(SKusrdatta);
-      localStorage.setItem("SKusrdatta", string);
-      console.log(SKusrdatta);
-    } else if (SKusrdatta.some((SKusrdatta) => SKusrdatta.time === et)) {
+    // This comapares the time to the times already saved in the div and changes the text of the object to match the new one
+    if (SKusrdatta.some((SKusrdatta) => SKusrdatta.time === et)) {
       for (var i = 0; i < SKusrdatta.length; i++) {
         if (SKusrdatta[i].time == et) {
           SKusrdatta[i].text = es;
@@ -121,24 +120,7 @@ $(function (event) {
           console.log(SKusrdatta);
         }
       }
-    } else if (SKusrdatta.some((SKusrdatta) => SKusrdatta.time !== et)) {
-      SKusrdatta.push(Timeblockval);
-      let string = JSON.stringify(SKusrdatta);
-      localStorage.setItem("SKusrdatta", string);
-      console.log(SKusrdatta);
     }
   });
   console.log(SKusrdatta);
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
